175e06ff68be19e7221e72a08990a8c765884667
IrinaFedotova86 <rnk86@mail.ru>
2024-09-27 13:41:33 +0300

    login negative

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,6 +1,8 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -12,9 +14,9 @@ public class HelperUser extends HelperBase{
     }
 
     public void openLoginForm(){
-        WebElement el = wd.findElement(By.xpath("//a[text()='Log in']"));
+        WebElement el = wd.findElement(By.xpath("//a[text()=' Log in ']"));
         el.click();
-        //click(By.xpath("//a[text()='Log in']"));
+        //click(By.xpath("//a[text()=' Log in ']"));
     }
 
     public void fillLoginForm(String email, String password) {
@@ -23,11 +25,69 @@ public class HelperUser extends HelperBase{
         type(By.id("password"), password);
     }
 
-    public void submitLogin() {
-        click(By.xpath("//button[text()='Y’alla!']"));
+    public void fillLoginForm(User user) {
+
+        type(By.id("email"), user.getEmail());
+        type(By.id("password"), user.getPassword());
+    }
+
+    public void submit() {
+        click(By.xpath("//*[@type='submit']"));
+        //click(By.xpath("//button[text()='Y’alla!']"));
     }
 
     public boolean isLogget() {
         return  isElementPresent(By.xpath("//h2[text()='Logged in success']"));
     }
+
+    public void clickOkButton() {
+        click(By.xpath("//button[text()='Ok']"));
+    }
+
+    public String getMessage() {
+       // WebElement el = wd.findElement(By.cssSelector(".dialog-container>h2"));
+       // String text = el.getText();
+       // return text;
+        pause(3000);
+        System.out.println(wd.findElement(By.cssSelector(".dialog-container>h2")).getText());
+        return wd.findElement(By.cssSelector(".dialog-container>h2")).getText();
+    }
+
+    public void pause(int time){
+        try {
+
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void logout() {
+        click(By.xpath("//*[text()=' Logout ']"));
+    }
+
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//*[text()=' Logout ']"));
+    }
+
+    public void openRegistrationForm() {
+        click(By.xpath("//a[text()=' Sign up ']"));
+    }
+
+    public void fillRegistrationForm(User user) {
+        type(By.id("name"), user.getFirstName());
+        type(By.id("lastName"), user.getLastName());
+        type(By.id("email"), user.getEmail());
+        type(By.id("password"), user.getPassword());
+    }
+
+    public void checkPolicy() {
+        //click(By.id("terms-of-use"));
+        //click(By.cssSelector("label[for='terms-of-use']"));
+        JavascriptExecutor js = (JavascriptExecutor) wd;
+        js.executeScript("document.querySelector('#terms-of-use').click()");
+    }
+
+
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,16 +1,92 @@
 package tests;
 
+import models.User;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends  TestBase{
 
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
+
     @Test
-    public void test(){
+    public void loginSuccess(){
         app.getHelperUser().openLoginForm();
         app.getHelperUser().fillLoginForm("rnk86@mail.ru", "QweAsd12$");
-        app.getHelperUser().submitLogin();
+        app.getHelperUser().submit();
 
         Assert.assertTrue(app.getHelperUser().isLogget());
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+       // app.getHelperUser().clickOkButton();
+    }
+
+    @Test
+    public void loginSuccessModel(){
+
+        //User user = new User().setEmail("margo@gmail.com").setPassword("Mmar123456$");
+
+
+        //user.setEmail("margo@gmail.com");
+        //user.setPassword("Mmar123456$");
+
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("rnk86@mail.ru", "QweAsd12$");
+        app.getHelperUser().submit();
+
+        Assert.assertTrue(app.getHelperUser().isLogget());
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+        //app.getHelperUser().clickOkButton();
+    }
+
+    @Test
+    public void loginWrongEmail(){
+
+        User user = new User().setEmail("rnk86@mailru").setPassword("QweAsd12$");
+
+
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submit();
+
+        Assert.assertEquals(app.getHelperUser().getMessage(), "\"Login or Password incorrect\"");
+
+    }
+
+    @Test
+    public void loginWrongPassword(){
+
+        User user = new User().setEmail("rnk86@mail.ru").setPassword("QweAsd312$");
+
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submit();
+
+        Assert.assertEquals(app.getHelperUser().getMessage(), "\"Login or Password incorrect\"");
+    }
+
+    @Test
+    public void loginUnredisteredUser(){
+
+        User user = new User().setEmail("marg@gmail.com").setPassword("Mmar12gtr$");
+
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submit();
+
+        Assert.assertEquals(app.getHelperUser().getMessage(), "\"Login or Password incorrect\"");
+    }
+
+
+
+    @AfterMethod
+    public void postCondition(){
+        app.getHelperUser().clickOkButton();
+
     }
 }
